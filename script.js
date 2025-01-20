953c1f8fc2f83d4bbd4739e2b4871a69a01a07a0
Ayushman <ayushmanchoudhary43@gmail.com>
2025-01-20 12:03:44 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let str="";// variable used for declaring results
 let btn=document.querySelectorAll(".button");// selecting all buttons with class button using DOM
-// creating array of buttons and also creating a event
+
 let userInput=document.querySelector("input");
 
+// creating array of buttons and also creating a event on the for 
 Array.from(btn).forEach((button)=>{
+  
     button.addEventListener('click',(e)=>{
       // code for evaluating expressions
       if(e.target.innerHTML=="="){
-        
-        str=eval(str);
+        str=eval(userInput.value);
         userInput.value=str;
       }
       else if(e.target.innerHTML=="X"){
