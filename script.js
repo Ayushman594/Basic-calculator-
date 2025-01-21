497bfd3be5999898bce70ec72095886266f4088c
Ayushman <ayushmanchoudhary43@gmail.com>
2025-01-21 11:38:53 +0530

    updated

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let str="";// variable used for declaring results
 let btn=document.querySelectorAll(".button");// selecting all buttons with class button using DOM
 
 let userInput=document.querySelector("input");
-
 // creating array of buttons and also creating a event on the button for performing operations 
 Array.from(btn).forEach((button)=>{
   
@@ -11,19 +10,27 @@ Array.from(btn).forEach((button)=>{
       if(e.target.innerHTML=="="){
         str=eval(userInput.value);
         userInput.value=str;
+        let M=userInput.value;
+        if(e.target.innerHTML=="M+"){
+          str=M+userInput.value;
+          userInput.value=str;
+        }
+        else if(e.target.innerHTML=="M-"){
+          str=M-userInput.value;
+          userInput.value=str;
+        }
+      }
+      else if(e.target.innerHTML=="DEL"){
+        str=userInput.value;
+        str=str.substring(0,str.length-1)
+        userInput.value=str;
       }
       else if (e.target.innerHTML=="C"){
         str="";
         userInput.value=str;
       }
-      else if(e.target.innerHTML=="DEL"){
-        str=str.substring(0,str.length-1)
-        userInput.value=str;
-      }
-      else if(e.target.innerHTML=="x2"){
-        str=userInput.value*userInput.value;
-        userInput.value=str;
-      }
+      
+      
       // else if(str==0){
       //   str=e.target.innerHTML;
       //   userInput.value=str;
